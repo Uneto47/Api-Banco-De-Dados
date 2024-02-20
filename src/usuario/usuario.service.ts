c666ff54011cff484bdd523e84aa4cd6e87fb128
Uneto47 <netovasconcelos02@gmail.com>
2024-02-19 22:20:45 -0300

    feat: add usuario services

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { Usuario } from './entities/usuario.entity';
 
 @Injectable()
 export class UsuarioService {
+  private usuarios: Usuario[] = [];
   create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+    const usuario: Usuario = new Usuario();
+    usuario.id = this.usuarios.length;
+    Object.assign(usuario, createUsuarioDto);
+    this.usuarios.push(usuario);
+
+    return usuario;
   }
 
   findAll() {
-    return `This action returns all usuario`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} usuario`;
-  }
-
-  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return `This action updates a #${id} usuario`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} usuario`;
+    return this.usuarios;
   }
 }
